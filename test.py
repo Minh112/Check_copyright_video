5d22f8a366e019e9b42392b2c53d9363c20930ee
Minh <minhdtq112@gmail.com>
2021-12-10 11:04:26 +0700

    second commit

--- test.py
+++ test.py
@@ -1,20 +1,21 @@
 import os
 import numpy as np
-from pyVideoProcess import extract_video_to_img
+from pyVideoProcess import extract_videos_to_npy
 from train import build_model
 from datetime import timedelta
 from util_copyright import remove_accents
 
 
 def predict_video(video_test, video_true):
-    rename = remove_accents(video_true)
+    rename_true = remove_accents(video_true)
+    rename_test = remove_accents(video_test)
     model = build_model()
-    model.load_weights("weight/"+rename+".h5")
-    folder_img = os.path.join("img/Test", rename)
+    model.load_weights("weight/"+rename_true+".h5")
+    folder_img = os.path.join("img/Test", rename_test)
     folder_videos = os.path.join("video/Test", video_test)
-    path_npy = "npy/{}.npy".format(rename)
+    path_npy = "npy/{}.npy".format(rename_test)
 
-    extract_video_to_img(folder_videos, folder_img, path_npy)
+    extract_videos_to_npy(folder_videos, folder_img, path_npy)
 
     data_test = np.load(path_npy)
     predict = model.predict(data_test)
@@ -34,13 +35,12 @@ def predict_video(video_test, video_true):
             max_value = averange
         key = f"{timedelta(seconds=int(time_start))}>{timedelta(seconds=int(time_end))}"
         result["warn"][key] = str(averange)  # {'0:00:45 -> 0:01:00': '0:00:45 -> 0:01:00'}
+    result["max"] = max_value
 
-        result["max"] = max_value
-
-        print(result)
+    print(result)
 
 
-#if __name__ == '__main__':
-    #predict_video(..........)
+if __name__ == '__main__':
+    predict_video("Người lạ Justatee", "SCho anh gần em thêm chút nữa")
 
 
